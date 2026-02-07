d0126fbdd13610ee2842f0a2f0cbf46a9a9ae213
112cserx <davya.golubev@mail.ru>
2026-02-08 00:27:38 +0800

    Stage 3: Visualization added

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import pandas as pd
 import json
+import matplotlib.pyplot as plt
+import seaborn as sns
 
 # Открываем файл вручную
 with open("events.json", "r") as f:
@@ -20,3 +22,26 @@ print(df.info())
 print("\nРаспределение событий по типам:")
 signature_counts = df["signature"].value_counts()
 print(signature_counts)
+
+# Настройка стиля (чтобы красиво выглядело)
+sns.set(style="whitegrid")
+
+# Размер графика
+plt.figure(figsize=(12, 6))
+
+# Построение графика
+sns.countplot(data=df, x="signature", order=df["signature"].value_counts().index)
+
+# Заголовок и подписи
+plt.title("Распределение типов событий информационной безопасности")
+plt.xlabel("Тип события")
+plt.ylabel("Количество")
+
+# Поворот подписей (иначе они не влезут)
+plt.xticks(rotation=90)
+
+# Чтобы всё аккуратно уместилось
+plt.tight_layout()
+
+# Показать график
+plt.show()
